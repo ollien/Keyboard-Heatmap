3f656f9102e5e24e240df3df50b8d4d328f4a2e4
Nick Krichevsky <njk828@gmail.com>
2014-07-26 14:34:37 -0400

    Remove image, begin work on manually drawing out keyboard, add calculation for percentage out of 255

--- src/HeatMapFrame.java
+++ src/HeatMapFrame.java
@@ -1,11 +1,52 @@
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.HashMap;
 
 /**
  * Created by Nick on 7/26/14.
  */
 public class HeatMapFrame extends JFrame {
+    boolean secondClick = false;
+    int lastX;
+    int lastY;
     HeatMapFrame(HashMap<Integer,Integer> data){
         this.add(new HeatMapPanel());
+
+        this.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                if (!secondClick) {
+                    lastX = mouseEvent.getX();
+                    lastY = mouseEvent.getY() - 20;
+                    System.out.println(lastX+","+lastY);
+                    secondClick=true;
+                }
+                else{
+                    System.out.println(mouseEvent.getX() - lastX +","+ (mouseEvent.getY() - lastY - 20));
+                    secondClick = false;
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+
+            }
+        });
     }
 }
--- src/HeatMapPanel.java
+++ src/HeatMapPanel.java
@@ -1,30 +1,60 @@
-import javax.imageio.ImageIO;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by Nick on 7/26/14.
  */
 public class HeatMapPanel extends JPanel {
-    BufferedImage image;
-
-    HeatMapPanel(){
-        try {
-            image = ImageIO.read(new File("keyboard.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    HashMap<Integer,Integer> data;
+    HeatMapPanel(HashMap<Integer,Integer> data){
+        this.data = data;
     }
-
+    //This function will take all the totals, and calculate them out of 255, which will be the Red value in the color code for the heatmap. The RGB values will be R,0,0
+    private HashMap<Integer,Integer> calculatePercentages(){
+        HashMap<Integer,Integer> tempData = new HashMap<Integer, Integer>();
+        int maxValue = getLargestKey();
+        for (int keyCode : data.keySet()){
+//            Calculate the proportion
+//            keyCode     x
+//            -------- = ---
+//            maxValue   255
+//            Which solves to keyCode*255 = maxValue*x
+//            Which isolated is equal to (keyCode*255)/maxValue
+            int value = keyCode*255/maxValue;
+            tempData.put(keyCode,value);
+        }
+        return tempData;
+    }
+    //Returns the percentage for the key with the largest value
+    private int getLargestKey(){
+        int max = 0;
+        for (int value : data.values()){
+            if (value>max)
+                max = value;
+        }
+        return max;
+    }
     @Override
     public void paint(Graphics g){
-        g.drawImage(image,0,0,null);
+        data = calculatePercentages();
+
+        //Escape key
+//        g.setColor(new Color())
+        g.fillRect(0, 0, 30, 30);
+
+        //Leave a 30 pixel space for the space between the escape key and the f row
+        //F row and print screen and scrlk and pause/break
+        for (int i=0; i<15; i++){
+            
+        }
+
     }
     @Override
     public Dimension getPreferredSize(){
-        return new Dimension(747,213);
+        return new Dimension(900,256);
     }
 }

13a1223ac11aca50af47065c78e5e08536aaf9f0
Nick Krichevsky <njk828@gmail.com>
2014-07-26 00:20:48 -0400

    Initial commit with global key listeners and Hashmap storing

---
+++ src/GlobalKeyListener.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Nick on 7/26/14.
+ */
+import org.jnativehook.keyboard.*;
+public class GlobalKeyListener implements NativeKeyListener{
+    public GlobalKeyListener()
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
+
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
+
+    }
+
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
+
+    }
+}

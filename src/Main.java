f28ceafcae7010e88d49c3d6578c769603b3eda6
Nick Krichevsky <njk828@gmail.com>
2014-07-27 01:30:44 -0400

    Add application frame

--- src/Main.java
+++ src/Main.java
@@ -8,31 +8,43 @@ import java.util.logging.LogManager;
 
 public class Main {
     private static HashMap<Integer,Integer> keyMap = new HashMap<Integer, Integer>();
+    private static boolean disableSpace = false;
     public static void main(String[] args) {
         try {
             GlobalScreen.registerNativeHook();
             LogManager.getLogManager().reset();
-            GlobalScreen.getInstance().addNativeKeyListener(new GlobalKeyListener(new Callback<Integer>() {
+            ApplicationFrame frame = new ApplicationFrame(new Callback<Boolean>() {
                 @Override
-                public void callback(Integer keyCode) {
-                    int currentValue = 0;
-                    if (keyMap.containsKey(keyCode)){
-                        currentValue = keyMap.get(keyCode);
-                    }
-                    keyMap.put(keyCode,currentValue+1);
-                    System.out.println(keyCode);
+                public void callback(Boolean spaces) {
+                    disableSpace = spaces;
+                    GlobalScreen.getInstance().addNativeKeyListener(new GlobalKeyListener(new Callback<Integer>() {
+                        @Override
+                        public void callback(Integer keyCode) {
+                            int currentValue = 0;
+                            if (keyMap.containsKey(keyCode)){
+                                if (disableSpace && keyCode!=NativeKeyEvent.VC_SPACE)
+                                    currentValue = keyMap.get(keyCode);
+                                else if (!disableSpace)
+                                    currentValue = keyMap.get(keyCode);
+                            }
+                            keyMap.put(keyCode,currentValue+1);
+                            System.out.println(keyCode);
+                        }
+                    }));
                 }
-            }));
+            },
+            new Callback<Void>() {
+                @Override
+                public void callback(Void aVoid) {
+                    HeatMapFrame p = new HeatMapFrame(keyMap);
+                    p.setVisible(true);
+                }
+            });
+            frame.setVisible(true);
         } catch (NativeHookException e) {
             e.printStackTrace();
         }
-//        try {
-//            Thread.sleep(30000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        HeatMapFrame p = new HeatMapFrame(keyMap);
-        p.setVisible(true);
+
         System.out.println("Hello World!");
 
     }
